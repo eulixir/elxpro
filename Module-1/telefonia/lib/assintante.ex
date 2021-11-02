913f65008fd5606bcf5da9d2e3fee8e2882468ff
João Pedro Alves <jotalmeida007@hotmail.com>
2021-11-02 16:49:31 -0300

    chore: class 2 completed

---
+++ Module-1/telefonia/lib/assintante.ex
@@ -0,0 +1,3 @@
+defmodule Assintante do
+  defstruct nome: nil, numero: nil, cpf: nil
+end
--- Module-1/telefonia/lib/telefonia.ex
+++ Module-1/telefonia/lib/telefonia.ex
@@ -1,5 +1,5 @@
 defmodule Telefonia do
-  def cadastrar_assintante(nome, _numero, _cpf) do
-    nome
+  def cadastrar_assintante(nome, numero, cpf) do
+    %Assintante{nome: nome, numero: numero, cpf: cpf}
   end
 end
