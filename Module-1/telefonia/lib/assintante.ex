31fe827717532395a65aacd8ed0af60668b2ca50
João Pedro Alves <jotalmeida007@hotmail.com>
2021-11-04 19:12:28 -0300

    chore: class 7 completed

--- Module-1/telefonia/lib/assintante.ex
+++ Module-1/telefonia/lib/assintante.ex
@@ -3,10 +3,41 @@ defmodule Assintante do
 
   @assinantes %{:pre_pago => "pre_pago.txt", :pos_pago => "pos_pago.txt"}
 
+  def buscar_assinante(numero, key \\ :all) do
+    buscar(numero, key)
+  end
+
+  defp buscar(numero, :pre_pago) do
+    assinantes_pre_pago()
+    |> Enum.find(assinantes(), &(&1.numero == numero))
+  end
+
+  defp buscar(numero, :pos_pago) do
+    assinantes_pos_pago()
+    |> Enum.find(assinantes(), &(&1.numero == numero))
+  end
+
+  defp buscar(numero, :all) do
+    assinantes()
+    |> Enum.find(assinantes(), &(&1.numero == numero))
+  end
+
+  def assinantes_pre_pago(), do: read(:pre_pago)
+  def assinantes_pos_pago(), do: read(:pos_pago)
+  def assinantes(), do: read(:pre_pago) ++ read(:pos_pago)
+
   def cadastrar(nome, numero, cpf, plano \\ :pre_pago) do
-    (read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}])
-    |> :erlang.term_to_binary()
-    |> write(plano)
+    case buscar_assinante(numero) do
+      nil ->
+        (read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}])
+        |> :erlang.term_to_binary()
+        |> write(plano)
+
+        {:ok, "Assinante #{nome} foi cadastrado! :)"}
+
+      _assinante ->
+        {:error, "Assinante com este número já cadastrado"}
+    end
   end
 
   defp write(lista_assinantes, plano) do
