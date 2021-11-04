423bccc8be066a5122d67f7497731984ba6e8dd9
João Pedro Alves <jotalmeida007@hotmail.com>
2021-11-04 18:29:39 -0300

    chore: class 4 completed

--- Module-1/telefonia/lib/assintante.ex
+++ Module-1/telefonia/lib/assintante.ex
@@ -1,18 +1,20 @@
 defmodule Assintante do
-  defstruct nome: nil, numero: nil, cpf: nil
+  defstruct nome: nil, numero: nil, cpf: nil, plano: nil
 
-  def cadastrar(nome, numero, cpf) do
-    (read() ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf}])
+  @assinantes %{:pre_pago => "pre_pago.txt", :pos_pago => "pos_pago.txt"}
+
+  def cadastrar(nome, numero, cpf, plano \\ :pre_pago) do
+    (read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}])
     |> :erlang.term_to_binary()
-    |> write()
+    |> write(plano)
   end
 
-  defp write(lista_assinantes) do
-    File.write("assinantes.txt", lista_assinantes)
+  defp write(lista_assinantes, plano) do
+    File.write(@assinantes[plano], lista_assinantes)
   end
 
-  def read() do
-    {:ok, assinantes} = File.read("assinantes.txt")
+  def read(plano) do
+    {:ok, assinantes} = File.read(@assinantes[plano])
 
     assinantes
     |> :erlang.binary_to_term()
--- Module-1/telefonia/lib/telefonia.ex
+++ Module-1/telefonia/lib/telefonia.ex
@@ -1,5 +1,10 @@
 defmodule Telefonia do
-  def cadastrar_assintante(nome, numero, cpf) do
-    Assintante.cadastrar(nome, numero, cpf)
+  def start() do
+    File.write("pre_pago.txt", :erlang.term_to_binary([]))
+    File.write("pos_pago.txt", :erlang.term_to_binary([]))
+  end
+
+  def cadastrar_assintante(nome, numero, cpf, plano) do
+    Assintante.cadastrar(nome, numero, cpf, plano)
   end
 end
