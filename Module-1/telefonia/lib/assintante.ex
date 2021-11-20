ec4dfd366f5535cb9bd17dbd4e64bcad45dee53e
joaopealves <jotalmeida007@hotmail.com>
2021-11-20 11:48:41 -0300

    chore: class 8 completed

--- Module-1/telefonia/lib/assintante.ex
+++ Module-1/telefonia/lib/assintante.ex
@@ -3,24 +3,12 @@ defmodule Assintante do
 
   @assinantes %{:pre_pago => "pre_pago.txt", :pos_pago => "pos_pago.txt"}
 
-  def buscar_assinante(numero, key \\ :all) do
-    buscar(numero, key)
-  end
+  def buscar_assinante(numero, key \\ :all), do: buscar(numero, key)
+  defp buscar(numero, :pre_pago), do: filtro(assinantes_pre_pago(), numero)
+  defp buscar(numero, :pos_pago), do: filtro(assinantes_pos_pago(), numero)
+  defp buscar(numero, :all), do: filtro(assinantes(), numero)
 
-  defp buscar(numero, :pre_pago) do
-    assinantes_pre_pago()
-    |> Enum.find(assinantes(), &(&1.numero == numero))
-  end
-
-  defp buscar(numero, :pos_pago) do
-    assinantes_pos_pago()
-    |> Enum.find(assinantes(), &(&1.numero == numero))
-  end
-
-  defp buscar(numero, :all) do
-    assinantes()
-    |> Enum.find(assinantes(), &(&1.numero == numero))
-  end
+  defp filtro(lista, numero), do: Enum.find(lista, &(&1.numero == numero))
 
   def assinantes_pre_pago(), do: read(:pre_pago)
   def assinantes_pos_pago(), do: read(:pos_pago)
@@ -45,9 +33,13 @@ defmodule Assintante do
   end
 
   def read(plano) do
-    {:ok, assinantes} = File.read(@assinantes[plano])
+    case File.read(@assinantes[plano]) do
+      {:ok, assinantes} ->
+        assinantes
+        |> :erlang.binary_to_term()
 
-    assinantes
-    |> :erlang.binary_to_term()
+      {:error, :ennoent} ->
+        {:error, "Arquivo inválido"}
+    end
   end
 end
